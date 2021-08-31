118cf9476412f1864cfe62e9d97c75baca894a4d
robbieW2004 <62378933+robbieW2004@users.noreply.github.com>
2021-08-31 11:29:02 +0200

    added the python scripts
    this is version 1

---
+++ 2.py
@@ -0,0 +1,18 @@
+import os
+import subprocess
+with open("output.txt") as f:
+    lines = f.readlines()
+    y = len(lines)
+    x = 0
+while x < y:
+    #print(lines)
+    filename = lines[x].rstrip()
+    print(x, filename)
+    program='NBTUtil.exe'
+    arguments=f'--path=C:\\Users\\...\\signextractor\\regions\\{filename}' # put the path where you have the region files like this C:\\Users\\*username*\\...... double dash is important
+    argument2='--printtree'
+    argument3=f'> C:\\Users\\...\\signextractor\\{filename}.txt' # put the path to the folder this program is in 
+    print(program, arguments, argument2, argument3)
+    with open(f'C:\\Users\\...\\signextractor\\{filename}.txt', "w") as outfile: # put the same path here that you did at line 14
+        subprocess.call([program, arguments, argument2], stdout=outfile)
+    x = x+1
