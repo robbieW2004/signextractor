7a6e65aa664e1666000f512a40ab7faa6ef1bb6e
robbieW2004 <62378933+robbieW2004@users.noreply.github.com>
2021-09-01 15:54:33 +0200

    made them functions kinda

--- 1.py
+++ 1.py
@@ -1,5 +1,14 @@
+import sys
 import os
-for root, dirs, files in os.walk("."):
-    for filename in files:
-        if ".mca" in filename:
-            print(filename)
+def listmcas():
+    original_stdout = sys.stdout
+    for root, dirs, files in os.walk("."):
+        for filenames in files:
+            if ".mca" in filenames:
+                print(filenames)
+                with open('output.txt', 'w') as outfile:
+                    sys.stdout = outfile
+                    for filenames in files:
+                        print(filenames)
+                    sys.stdout = original_stdout
+listmcas()
--- 3.py
+++ 3.py
@@ -1,12 +1,14 @@
-import os
-for root, dirs, files in os.walk("."):
-    for filename in files:
-        if ".mca.txt" in filename:
-            N = 4
-            with open(filename, 'r') as f:
-                lines = f.read().splitlines()
-                for i, line in enumerate(lines):
-                    if "id: minecraft:sign" in line:
-                        j = i-N if i>N else 0
-                        for k in range(j,i + 5):
-                            print(lines[k])
+def extractsigns():
+    import os
+    for root, dirs, files in os.walk("."):
+        for filename in files:
+            if ".mca.txt" in filename:
+                N = 4
+                with open(filename, 'r') as f:
+                    lines = f.read().splitlines()
+                    for i, line in enumerate(lines):
+                        if "id: minecraft:sign" in line:
+                            j = i-N if i>N else 0
+                            for k in range(j,i + 5):
+                                print(lines[k])
+extractsigns()                                
